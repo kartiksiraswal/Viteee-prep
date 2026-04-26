55f80af0c3eab526852bdf56c3f8a3bd137f87fe
kartiksiraswal <mail.kartik2608@gmail.com>
2026-04-26 09:15:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import VITEEEMock from "./pages/VITEEEMock";
+import AdminDashboard from "./pages/AdminDashboard";
 
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/test" element={<VITEEEMock />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
