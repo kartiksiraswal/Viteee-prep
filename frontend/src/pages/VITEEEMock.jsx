87a51da187d61941f69ceba2aeea4ac0f00a16d4
Kartik Pal <mail.kartik2608@gmail.com>
2026-04-30 09:27:30 +0530

    Update VITEEEMock.jsx

--- frontend/src/pages/VITEEEMock.jsx
+++ frontend/src/pages/VITEEEMock.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import API_URL from "../api"; 
 import { Card, CardContent } from "../components/ui/card";
 import { Button } from "../components/ui/button";
 
